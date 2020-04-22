1cd329966f6103ac1e719fd668910b407facb6cc
meilixiaozhang <redpangci@gmail.com>
2020-04-22 20:00:55 +1000

    spread prev array to new array

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,20 +1,36 @@
-import React from "react";
+import React, { useState } from "react";
 
 function App() {
+  const [content, setContent] = useState("");
+  const [items, setItems] = useState([]);
+
+  function handleClick() {
+    setItems((prevItems) => {
+      return [...prevItems, content];
+    });
+    setContent("");
+    console.log(items);
+  }
+
+  function handleChange(event) {
+    setContent(event.target.value);
+  }
   return (
     <div className="container">
       <div className="heading">
         <h1>To-Do List</h1>
       </div>
       <div className="form">
-        <input type="text" />
-        <button>
+        <input onChange={handleChange} type="text" value={content} />
+        <button onClick={handleClick}>
           <span>Add</span>
         </button>
       </div>
       <div>
         <ul>
-          <li>A Item </li>
+          {items.map((item) => (
+            <li>{item}</li>
+          ))}
         </ul>
       </div>
     </div>
